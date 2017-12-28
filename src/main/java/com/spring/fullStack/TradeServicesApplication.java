b059c1e945ca1888994355b289d3ec04ef43e1a7
Sahil Chopra <scho20@sapient.com>
2017-12-28 11:26:56 +0530

    first commit

---
+++ src/main/java/com/spring/fullStack/TradeServicesApplication.java
@@ -0,0 +1,12 @@
+package com.spring.fullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeServicesApplication.class, args);
+	}
+}
